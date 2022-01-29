a721ead030c57983dbe09c81eb54b283294a7f0f
hff2 <hophoebeho@gmail.com>
2022-01-29 16:27:19 +0800

    feat: add FeedbackList、FeedbackItem components and fake data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 import Header from './components/Header';
 
+import { useState } from 'react';
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback}/>
+      </div>
     </>
   );
 }
