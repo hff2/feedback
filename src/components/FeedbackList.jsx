d33a5e84e087eb3b8b4bf2f281f34f65ca191feb
hff2 <hophoebeho@gmail.com>
2022-01-29 22:25:00 +0800

    feat: add feedback item fade animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList({feedback,handleDelete}) {
     if(!feedback || feedback.length === 0){
@@ -9,11 +10,15 @@ function FeedbackList({feedback,handleDelete}) {
 
     return (
       <div className="feedback-list">
-        {
-          feedback.map((item)=>(
-              <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-          ))
-        }
+        <AnimatePresence>
+          {
+            feedback.map((item)=>(
+              <motion.div key={item.id} initial={{option: 0}} animate={{opacity:1}} exit={{opacity:0}}>
+                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+              </motion.div>
+            ))
+          }
+        </AnimatePresence>
       </div>
     )
 }
