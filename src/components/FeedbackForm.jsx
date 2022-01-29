63ec6dc7b35cdf2463388e4d7ac4941f8d8a7275
hff2 <hophoebeho@gmail.com>
2022-01-29 18:52:09 +0800

    feat: add real-time input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
     const [text,setText] = useState('')
+    const [btnDisabled,setBtnDisabled] = useState(true)
+    const [message,setMessage] = useState('')
 
     const handleTextChange = (e) =>{
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 8){
+            setMessage('請填入至少八個字')
+            setBtnDisabled(true)
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -16,8 +28,9 @@ function FeedbackForm() {
                 {/* @todo - 分數選項 */}
                 <div className="input-group">
                     <input type="text" placeholder="Write a review" onChange={handleTextChange} value={text}/>
-                    <Button type="submit">Send</Button>
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )}
